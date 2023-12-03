a7f03aef2479002569f610b6ce43b4706f5ee445
Janina Szkut <janina.szkut@gmail.com>
2023-12-02 23:59:14 -0500

    Day 1, part 1

---
+++ day_1/part_1.rb
@@ -0,0 +1,10 @@
+# input = %w{ 1abc2 pqr3stu8vwx a1b2c3d4e5f treb7uchet}
+input = File.read("input.txt").split
+
+solution = input.reduce(0) do |memo, word|
+	new_word = word.delete "a-z"
+	code = (new_word[0] + new_word[-1]).to_i
+	memo + code
+end
+
+puts solution
